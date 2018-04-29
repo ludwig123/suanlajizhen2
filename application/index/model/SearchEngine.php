3aecf3a5ec343c6023a890f4f8c10f53febc6ca2
ludwig123 <hejingjizhen@gmail.com>
2018-04-29 17:43:51 +0800

    add 车牌查询

--- application/index/model/SearchEngine.php
+++ application/index/model/SearchEngine.php
@@ -3,16 +3,49 @@ namespace app\index\model;
 
 use think\Db;
 
-class SearchEngine{
-    public function law_fa($index){
-       $result =  Db::name('law_fa')->where('id',$index)->find();
-       if ($result == NULL){
-           return "";
-       }
-       return $result;
+class SearchEngine
+{
+
+    public function law_fa($index)
+    {
+        $max = $this->count_table_row("law_fa");
+        
+        if ($index < 1) {return "index should >= 1";} 
+        
+        if ($index > $max) {return "index should <= " . $max;}
+        
+        if ($index <= $max && $index >= 1) {
+            $result = Db::name('law_fa')->where('id', $index)->find();
+            return $result;
+        }
     }
     
-    private function law_count(){
+    public function codeSearch($code){
         
+    }
+
+    private function count_table_row($name)
+    {
+        $result = db($name)->select();
+        return count($result);
+    }
+    
+    
+    public function connectBus($chePai){
+        
+        $map ['车牌号'] = array('like', '%'.$chePai.'%');
+        
+        $temp = db( 'connect_bus' )->where ('车牌号', 'like', '%'.$chePai.'%')->select ();
+        
+        if ($temp == null){
+            return $chePai."未查询到接驳信息";
+        }
+        
+        $content = null;
+        foreach ( $temp[0] as $k => $v ) {
+            $content = $content . $k . '：' . $v . "\n";
+        }
+        
+        return $content;
     }
 }
--- tests/SearchEngineTest.php
+++ tests/SearchEngineTest.php
@@ -41,17 +41,37 @@ class SearchEngineTest extends TestCase
     
     public function testLaw_fa()
     {
-        $index1 = "1";
+        $index1 = 1;
         
        $result =  $this->searchEngine->law_fa($index1);
-       var_dump($result);
+//        var_dump($result);
 //         $this->assertNull("000","不应该是空的");
         $this->assertEquals(1, $result['id']);
         
         $index200 = "200";
         $result =  $this->searchEngine->law_fa($index200);
-        $this->assertNull($result);
+        $this->assertContains("index should <=", $result);
+        
+        $indexless = -11;
+        $result =  $this->searchEngine->law_fa($indexless);
+        $this->assertContains("index should >= 1", $result);
         
     }
+    
+    public function testConnectBusFind(){
+        $chePai = "川C19485";
+        $result = $this->searchEngine->connectBus($chePai);
+        $this->assertContains($chePai, $result);
+        
+        
+    }
+    
+    public function testConnecBusNotFind(){
+        $chePai = "湘D99999";
+        $result = $this->searchEngine->connectBus($chePai);
+        $this->assertContains("未查询到接驳信息", $result);
+    }
+    
+    
 }
 
